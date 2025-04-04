5b5446c2f2e41bb21fd3a9e031700313313d9c75
nnamdi <uzonnamdi31@gmail.com>
2025-04-04 23:58:19 +0100

    updated the card component

--- src/components/home/Cards.jsx
+++ src/components/home/Cards.jsx
@@ -1,39 +1,32 @@
-import React from 'react'
-import { cardsArray } from './Array'
-import Image from 'next/image'
-import Link from 'next/link'
-import { useRecoilState } from 'recoil'
-import { myMovieList } from '@/store/atom/movielist'
-
+import React from 'react';
+import { useRecoilState } from 'recoil';
+import { myMovieList } from '@/store/atom/movielist';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Cards = () => {
-  const [movie, setMovie] = useRecoilState(myMovieList)
-
+  const [movie, setMovie] = useRecoilState(myMovieList);
 
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-[3.083vw] px-20'>
-      { movie !== null && (
+      {movie !== null &&
         movie.map((item) => (
-          <Link href={`/movie/${item.id}`}>
-            <div key={item.id} className='flex flex-col items-center gap-[0.903vw]'>
-                <Image 
-                  src={item.image} 
-                  alt='alt' 
-                  width={250} 
-                  height={375} 
-                  // className='w-[17.35vw] h-[26.0417vw] hover:opacity-[70%] cursor-pointer'
-
+          <div key={item.id}>
+            <Link href={`/movie/${item.id}`}>
+              <div className='flex flex-col items-center gap-[0.903vw]'>
+                <Image
+                  src={item.image}
+                  alt='alt'
+                  width={250}
+                  height={375}
                 />
                 <p className='text-white font-[1.389vw]'>{item.title}</p>
-            </div>
-          
-          </Link>
-
-        ))
-      )
-      }
+              </div>
+            </Link>
+          </div>
+        ))}
     </div>
-  )
-}
+  );
+};
 
-export default Cards
+export default Cards;
