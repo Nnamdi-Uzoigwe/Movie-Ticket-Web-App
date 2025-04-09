fc15066a08c4f2e65f94f060bc1d88ec31d78159
nnamdi <uzonnamdi31@gmail.com>
2025-04-09 13:39:41 +0100

    px-12

--- src/app/seatselector/SeatSelection.js
+++ src/app/seatselector/SeatSelection.js
@@ -147,7 +147,7 @@ let total;
 
   if (showError) {
     return (
-      <div className="fixed inset-0 bg-black bg-opacity-80 flex items-center justify-center z-50">
+      <div className="fixed inset-0 bg-black bg-opacity-80 flex items-center justify-center z-50 px-12">
         <div className="bg-white p-6 rounded-lg max-w-md text-center">
           <h2 className="text-2xl font-bold text-red-600 mb-4">Unauthorized Access</h2>
           <p className="text-gray-700 mb-4">
